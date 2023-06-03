2578680c094bec961f30b05e44e32251a25b05db
cent0 <39629876+cent0@users.noreply.github.com>
2023-06-03 17:19:21 +0200

    Create EspMeterGtil2new.cpp
    // Dave Demos 05/28/2023 thank you for news
    // EspMeterGtil2new.cpp
    // new testing for Transmitter with Simple CODE 
    // the device transmitt Direct to smarphone or Tablet / pc
    // if new Programmer have sugestion for increase stability code plese help!
    // to nex release if possible add Mqtt for sending data to any server.
    // github Cent0
    // https://www.youtube.com/@s-max3277

---
+++ EspMeterGtil2new.cpp
@@ -0,0 +1,8 @@
+// Dave Demos 05/28/2023 thank you for news
+// EspMeterGtil2new.cpp
+// new testing for Transmitter with Simple CODE 
+// the device transmitt Direct to smarphone or Tablet / pc
+// if new Programmer have sugestion for increase stability code plese help!
+// to nex release if possible add Mqtt for sending data to any server.
+// github Cent0
+// https://www.youtube.com/@s-max3277
